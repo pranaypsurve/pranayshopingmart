5a81428888ad50e58b42a5866d39f843a756f1d4
Pranay Surve <pranaysurve75@gmail.com>
2022-07-22 12:07:19 +0530

    google tag and anylitcs setup

--- components/header.jsx
+++ components/header.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 function Header() {
     const router = useRouter();
     const currentRoute = router.pathname;
   return (
+    <>
+    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+    <Script id='google-analytics-script' strategy="beforeInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+    </Script>
+    
+    <Script strategy="beforeInteractive">
+        {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TCF9LSF');
+        `} 
+    </Script>
     <nav className="shopping_mart_navbar navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
             <Link href='/'>
@@ -34,6 +57,8 @@ function Header() {
             </div>
         </div>
     </nav>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCF9LSF" height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
+    </>
   )
 }
 export async function getStaticProps() {
